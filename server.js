8d6f49cd4e36c9dd0fc4bcbdf967dccb07bdf1e8
Leannaland <leannaland2011@gmail.com>
2020-11-08 18:49:39 -0500

    note file

---
+++ server.js
@@ -0,0 +1,60 @@
+// ==============================================================================
+// DEPENDENCIESn
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var path = require("path");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
+
+
+// POST method route
+
+
+   app.post('/notes', function (req, res) {
+     res.send("public/notes.html")
+ })
+
